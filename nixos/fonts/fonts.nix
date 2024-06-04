3b4cc25e9c3e8eb4e04d406e4ca852989ee7f9c1
Bathys Scarf <68154148+bathys@users.noreply.github.com>
2024-06-04 23:09:29 +0900

    Create fonts.nix

---
+++ nixos/fonts/fonts.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+{
+  fonts = {
+    packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    fira-code
+    jetbrains-mono
+    font-awesome
+    terminus_font
+    twemoji-color-font
+    nerdfonts
+    ];
+    fontDir.enable = true;
+    fontconfig = {
+      defaultFonts = {
+         serif = [ "Noto Serif CJK JP" "Noto Color Emoji"  ];
+         sansSerif = ["Noto Sans CJK JP" "Noto Color Emoji"];
+         monospace = ["ComicShannsMono Nerd Font" "Noto Color Emoji"];
+         emoji = ["Noto Color Emoji"];
+      };
+    };
+  };
+}
