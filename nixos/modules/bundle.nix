c75ed4ceaeb90255e3379cbac1fa258814954988
Bathys Scarf <68154148+bathys@users.noreply.github.com>
2024-06-08 23:02:24 +0900

    Update bundle.nix

--- nixos/modules/bundle.nix
+++ nixos/modules/bundle.nix
@@ -3,17 +3,18 @@
     ./bluetooth.nix
     ./bootloader.nix
     ./env.nix
-    ./hyprland.nix
     ./network.nix
+    ./nextcloud.nix
     ./nixvim/nixvim.nix
+    ./opentabletdriver.nix
     ./security.nix
     ./services.nix
     ./sound.nix
     ./systemd.nix
-    ./thunar.nix
     ./trim.nix
     ./user.nix
     ./virtmanager.nix
+    ./xdg-portal.nix
     ./zram.nix
   ];
 }
