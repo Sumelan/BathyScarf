6c2427192f4021250be9ef2f287dd5d59c83508f
Bathys Scarf <bathys@proton.me>
2024-06-19 06:47:56 +0900

    Update hypridle.nix

--- home-manager/modules/wms/hypridle.nix
+++ home-manager/modules/wms/hypridle.nix
@@ -14,7 +14,7 @@
           on-timeout = "brightnessctl -s set 10";    # set monitor backlight to minomum, avoid 0 on OLED monitor.
           on-resume = "brightnessctl -r";            # monitor backlight restor.
         }
-# turn off keyboard backlight, uncomment this section if have keyboard backlight.
+  turn off keyboard backlight, uncomment this section if have keyboard backlight.
         {
           timeout = 150;                                           # 2.5 min
           on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0"; # turn off keyboard backlight.
@@ -23,14 +23,14 @@
         {
           timeout = 600;                        # 10 min.
           on-timeout = "loginctl lock-session";  # lock screen when timeout has passed.
-        } 
+        }
         {
            timeout = 680;                            #10.5 min.
            on-timeout = "hyprctl dispatch dpms off";  #screen off when timeout has passed.
            on-resume = "hyprctl dispatch dpms on";    #screen on when activity is detected after timeout has fired.
         }
         {
-          timeout = 18000;                  # 5 h.
+          timeout = 3600;                  # 1 h.
           on-timeout = "systemctl suspend";  # suspend pc.
         }
       ];
