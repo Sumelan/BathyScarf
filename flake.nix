efa32451af29c0b7419d91e440ec2113c472a963
Bathys Scarf <bathys@proton.me>
2024-06-19 06:07:46 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,24 +4,20 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    spicetify-nix.url = "github:the-argus/spicetify-nix";
     nix-colors.url = "github:misterio77/nix-colors";
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1"; # unstable hyprland
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-      };
-   #ags.url = "github:Aylur/ags"; 
-    home-manager = {
-      url = "github:nix-community/home-manager";
+    disko = {
+      url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    spicetify-nix.url = "github:the-argus/spicetify-nix";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = { nixpkgs, home-manager, spicetify-nix, ... }@inputs:
 
@@ -29,11 +25,11 @@
       system = "x86_64-linux";
 
       # User Variables
-      hostname = "Rin";
+      hostname = "BathyScarf";
       username = "bathys";
       gitUsername = "bathys";
-      gitEmail = "68154148+bathys@users.noreply.github.com";
-      theme = "rose-pine";
+      gitEmail = "bathys@proton.me";
+      theme = "catppuccin-macchiato";
 
       pkgs = import nixpkgs {
         inherit system;
@@ -58,7 +54,7 @@
             inputs.nixvim.nixosModules.nixvim
             inputs.disko.nixosModules.disko
             home-manager.nixosModules.home-manager
-            {
+              {
               home-manager.extraSpecialArgs = {
                 inherit username;
                 inherit gitEmail;
@@ -72,7 +68,7 @@
               home-manager.useUserPackages = true;
               home-manager.backupFileExtension = "backup";
               home-manager.users.${username} = import ./home-manager/home.nix;
-            }
+            } 
           ];
         };
       };
