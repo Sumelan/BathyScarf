5d756a4106658c67bb9a38f966c7465936ae5dd3
installer <installer@gmail.com>
2024-08-19 23:02:05 +0900

    init

--- home-manager/modules/wms/hyprland.nix
+++ home-manager/modules/wms/hyprland.nix
@@ -26,14 +26,13 @@
       #start programs
       exec-once = dbus-update-activation-environment --systemd --all
       exec-once = systemctl --user import-environment QT_QPA_PLATFORMTHEME WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-      exec-once = swww-daemon & swww img ~/Pictures/wallpapers/honkai-kafka-06.png
+      exec-once = swww-daemon & swww img ~/Pictures/wallpapers/genshin_ayaka.png
       exec-once = pkill dunst && Sleep .5 && dunst
       exec-once = eww daemon && sleep .5
       exec = eww open bar && sleep .5 && eww reload &
       exec-once = nm-applet
       exec-once = wl-paste --type text --watch cliphist store & wl-paste --type image --watch cliphist store & wl-paste --watch cliphist store
       exec-once = systemd
-      exec-once = spotify && sleep .5
       exec-once = kdeconnect-indicator
       exec-once = fcitx5
 
