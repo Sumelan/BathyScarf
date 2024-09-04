5c8522e7f59a04c0dce3d83a4185959b7f739ddd
bathys <bathys@proton.me>
2024-09-04 22:05:49 +0900

    back to previous state but cant switch because of fcitx5...

--- flake.nix
+++ flake.nix
@@ -18,16 +18,12 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-cosmic = {
-      url = "github:lilyinstarlight/nixos-cosmic";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, nixos-cosmic, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
 
     let
       system = "x86_64-linux";
@@ -49,13 +45,6 @@
             ./nixos/configuration.nix
             inputs.stylix.nixosModules.stylix
             inputs.disko.nixosModules.disko
-            {
-              nix.settings = {
-                substituters = [ "https://cosmic.cachix.org/" ];
-                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-              };
-            }
-            nixos-cosmic.nixosModules.default
             home-manager.nixosModules.home-manager
               {
               home-manager.extraSpecialArgs = {
