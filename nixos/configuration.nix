ac4d3fb3211ad6d9bfd44d183b66f8989c17937a
Su melan <bathys@proton.me>
2024-11-26 17:47:06 +0900

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -56,25 +56,30 @@
  services.getty.autologinUser = "sumelan";
 
   # Network
-  networking.networkmanager.enable = true;
-  services.openssh = {
-    enable = true;
-  # Add your SSH public key(s) here, if you plan on using SSH to connect.
-  # authorizedKeys.keys = [ ];
-    settings = {
-      PermitRootLogin = "no";
-  # Change to false if you want to SSH using public key(s). (recommended)
-      PasswordAuthentication = true;
+  networking = {
+    networkmanager.enable = true;
+    firewall = {
+      enable = true;
+      allowedUDPPorts = [];
+      allowedTCPPorts = [ 80 443 ];
     };
   };
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 80 443 ];
+  services = {
+    openssh = {
+      enable = true;
+  # Add your SSH public key(s) here, if you plan on using SSH to connect.
+      authorizedKeys.keys = [ ];
+      settings = {
+        PermitRootLogin = "no";
+  # Change to false if you want to SSH using public key(s). (recommended)
+        PasswordAuthentication = true;
+      };
+    };
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
   };
 
   # Security
