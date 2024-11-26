5a3be2cfaa01d5d07fe61fa16237f0def543833f
Su melan <bathys@proton.me>
2024-11-26 18:33:46 +0900

    Update fonts.nix

--- nixos/fonts/fonts.nix
+++ nixos/fonts/fonts.nix
@@ -11,7 +11,7 @@
       defaultFonts = {
          serif = [ "Noto Serif CJK JP" "Noto Color Emoji"  ];
          sansSerif = ["Noto Sans CJK JP" "Noto Color Emoji"];
-         monospace = ["ComicShannsMono Nerd Font" "Noto Color Emoji"];
+         monospace = ["Noto Color Emoji"];
          emoji = ["Noto Color Emoji"];
       };
     };
