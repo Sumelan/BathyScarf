6058b26c3c9f78ecdd11b353f86d09b685987a43
Su melan <bathys@proton.me>
2024-11-26 18:15:24 +0900

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -218,6 +218,57 @@
   	'';
   };
 
+# Sound
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+    jack.enable = true;
+    /*
+    extraConfig.pipewire.adjust-sample-rate = {
+      "context.properties" = {
+        "default.clock.rate" = 96000;
+        "defautlt.allowed-rates" = [ 192000  96000 48000 44100 ];
+        "default.clock.quantum" = 1024;
+        "default.clock.min-quantum" = 32;
+        "default.clock.max-quantum" = 2048;
+      };
+    };
+    */
+  };
+  # Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+        Name = "Hello";
+        ControllerMode = "dual";
+        FastConnectable = "true";
+        Experimental = "true";
+      };
+      Policy = {
+        AutoEnable = "true";
+      };
+    };
+  };
+  services.blueman.enable = true;
+
+  # Sane
+  hardware.sane = {
+    enable = true;
+    extraBackends = [ pkgs.sane-airscan ];
+    disabledDefaultBackends = [ "escl" ];
+  };
+  # Extra Logitech Support
+  hardware.logitech.wireless.enable = true;
+  hardware.logitech.wireless.enableGraphical = true;
+  # OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
   # zram
   zramSwap = {
     enable = true;
