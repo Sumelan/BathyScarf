31492242dcf62acdb03c9c0508e2ed3ad0d8ffb4
Su melan <bathys@proton.me>
2024-11-26 21:32:11 +0900

    Update hyprland.nix

--- home-manager/modules/hypr/hyprland.nix
+++ home-manager/modules/hypr/hyprland.nix
@@ -180,15 +180,15 @@
       bind = ALT, Tab, cyclenext
       bind = ALT, Tab, bringactivetotop
 
-      bind = ,XF86AudioRaiseVolume, exec, changevolume up
-      bind = ,XF86AudioLowerVolume, exec, changevolume down
-      binde = ,XF86AudioMute, exec, changevolume mute
+      bind = ,XF86AudioRaiseVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+      bind = ,XF86AudioLowerVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+      binde = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
       bind = ,XF86AudioPlay, exec, playerctl play-pause
+      bind = ,XF86AudioPause, exec, playerctl play-pause
       bind = ,XF86AudioNext, exec, playerctl next
       bind = ,XF86AudioPrev, exec, playerctl previous
-      bind = ,XF86MonBrightnessUp, exec, changebrightness up
-      bind = ,XF86MonBrightnessDown, exec, changebrightness down
-      bind = ,Print, exec, screenshotmenu
+      bind = ,XF86MonBrightnessDown,exec,brightnessctl set 5%-
+      bind = ,XF86MonBrightnessUp,exec,brightnessctl set +5%
 
 /*
       bind = ,Menu, exec, eww open --toggle dash
