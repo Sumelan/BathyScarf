78d3e2b636f398cdd1eade3a5893615d89528869
sumelan <bathys@proton.me>
2024-12-18 22:42:28 +0900

    rework hypridle.nix

--- modules/home/hypr/hypridle.nix
+++ modules/home/hypr/hypridle.nix
@@ -24,12 +24,12 @@
           on-timeout = "hyprctl dispatch dpms off";  #screen off when timeout has passed.
           on-resume = "hyprctl dispatch dpms on";    #screen on when activity is detected after timeout has fired.
         }
-        (if (host == "Rei") then ''
+        (if (host == "Rei") then 
           {
             timeout = 1200;                          # 20 min.
             on-timeout = "systemctl suspend";        # suspend pc.
           }
-        '' else {})
+         else {})
       ];
     };
   };
