d800b261bf4409fa580beb67eb05e8735987d6b6
sumelan <bathys@proton.me>
2024-12-28 11:12:23 +0900

    add display settings in swayosd

--- modules/home/services/wayland/swayosd.nix
+++ modules/home/services/wayland/swayosd.nix
@@ -1,7 +1,10 @@
-{ pkgs, config, ... }:
+{ host, ... }:
 {
   services.swayosd = {
     enable = true;
+    display = (if (host == "Rin") then ''
+      "HDMI-A-1"
+    '' else "eDP-1");
     topMargin = 1.0;
   };
 }
