ef385525b98f28190a5441d470ffb8323754201b
sumelan <bathys@proton.me>
2024-12-30 09:11:49 +0900

    add joplin settings to make same between devices.

--- modules/home/editors/joplin.nix
+++ modules/home/editors/joplin.nix
@@ -7,8 +7,50 @@
     sync.interval = "10m";
     general.editor = "${pkgs.helix}/bin/hx";
     extraConfig = {
-      "markdown.plugin.mark" = true;
+      editor.codeView = true;
+      richTextBannerDismissed = true;
+      locale = "ja_JP";
+      dateFormat = "YYYY-MM-DD";
+      "ocr.enabled" = true;
+      theme = 2;
+      themeAutoDetect = true;
+      preferredDarkTheme = 2;
+      layoutButtonSequence = 2;
       newNoteFocus = "title";
+      "markdown.plugin.softbreaks" = false;
+      "markdown.plugin.typographer" = false;
+      "markdown.plugin.mark" = true;
+      showTrayIcon = true;
+      startMinimized = false;
+      "ui.layout" = {
+        key = "root";
+        children = [
+          {
+            key = "sideBar";
+            width = 188;
+            visible = true;
+          }
+          {
+            key = "noteList";
+            width = 250;
+            visible = true;
+          }
+          {
+            key = "editor";
+            visible = true;
+          }
+        ];
+        visible = true;
+      };
+      autoUploadCrashDumps = true;
+      "sync.interval" = 600;
+      noteVisiblePanes = [
+        "editor"
+      ];
+      "editor.spellcheckBeta" = true;
+      "spellChecker.languages" = [
+        "en-US"
+      ];
     };
   };
 }
